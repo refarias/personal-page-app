4abb711532f1509465c304a3fe8f0a1a03893504
renanfarias <renanfarias.pt@gmail.com>
2023-06-24 12:33:36 +0100

    creating card component and planning sidebar menu

---
+++ src/app/card/card.component.ts
@@ -0,0 +1,14 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-card',
+  templateUrl: './card.component.html',
+  styleUrls: ['./card.component.sass']
+})
+export class CardComponent {
+
+  @Input("post") post:any;
+
+  
+
+}
